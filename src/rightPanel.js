5fffc5ca99567371c38b9edbf553b424bb4d2632
Amanshu <amanshukataria@hotmail.com>
2019-06-14 16:50:34 +0530

    scroll history to bottom

--- src/rightPanel.js
+++ src/rightPanel.js
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-function RightPanel({ history }) {
-  return (
-    <div className="rightPanel">
-      <div className="movesLabel">Your Moves</div>
-      <div className="allMoves">
-        {history.map((item, i) => {
-          return (
-            <div className="moveItem" key={i}>
-              Tile {item.tile} moved from {item.fromRow + 1},{item.fromCol + 1}{' '}
-              to {item.toRow + 1},{item.toCol + 1}.
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
-}
+class RightPanel extends PureComponent {
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
 
+  scrollToBottom = () => {
+    this.history.scrollTop = this.history.scrollHeight;
+  };
+
+  render() {
+    return (
+      <div className="rightPanel">
+        <div className="movesLabel">Your Moves</div>
+        <div
+          className="allMoves"
+          ref={el => {
+            this.history = el;
+          }}
+        >
+          {this.props.history.map((item, i) => {
+            return (
+              <div className="moveItem" key={i}>
+                Tile {item.tile} moved from {item.fromRow + 1},
+                {item.fromCol + 1} to {item.toRow + 1},{item.toCol + 1}.
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
 RightPanel.propTypes = {
   history: PropTypes.array.isRequired
 };
